bb1872056cb19d70af30f8b32e1c6b07fec7b456
Troy Fortin <troy@Troy-Jrs-MacBook-Pro.local>
2020-05-27 10:01:05 -0400

    Updates

---
+++ precache-manifest.5624919c6f2df43e656cabd05fff32d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13721d4385afdbb978cb4d2fa514da8",
+    "url": "/graphic-verify/index.html"
+  },
+  {
+    "revision": "198b649a0daadcecf1aa",
+    "url": "/graphic-verify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d9c3656bdc19989722b",
+    "url": "/graphic-verify/static/js/2.aedfeabf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/graphic-verify/static/js/2.aedfeabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198b649a0daadcecf1aa",
+    "url": "/graphic-verify/static/js/main.7cdd907c.chunk.js"
+  },
+  {
+    "revision": "492af4b2d4031ba77ee7",
+    "url": "/graphic-verify/static/js/runtime-main.1305b58b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/graphic-verify/static/media/logo.5d5d9eef.svg"
+  }
+]);
